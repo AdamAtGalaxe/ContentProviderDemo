c367d04887923c72097b862a0a72aa1a8ce703fa
AdamAtGalaxe <95375208+AdamAtGalaxe@users.noreply.github.com>
2021-12-09 12:50:26 -0500

    cleaned up

--- app/src/main/java/com/galaxe/contentproviderdemo/MainActivity.kt
+++ app/src/main/java/com/galaxe/contentproviderdemo/MainActivity.kt
@@ -16,67 +16,42 @@ class MainActivity : AppCompatActivity()
         ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
         ContactsContract.CommonDataKinds.Phone.NUMBER,
         ContactsContract.CommonDataKinds.Phone._ID
-
     ).toTypedArray()
 
     //using listview to show your contacts in list form
     lateinit var list_view:ListView;
 
-
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //Binding ListView to a variable
-        //to display the contact list
-
+        //Binding ListView to a variable to display the contact list
         list_view = findViewById(R.id.contact_list_view);
 
-        //Checking if the user has granted
-        //permission to access his contacts or not
-
-        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.READ_CONTACTS) !=
-            PackageManager.PERMISSION_GRANTED)
-        {
-            //If permission not granted
-            //then seek permission from user
-
-            ActivityCompat.requestPermissions(this,
-                Array(1){android.Manifest.permission.READ_CONTACTS},
-                100)
+        //Getting permissions
+        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, Array(1){ android.Manifest.permission.READ_CONTACTS},100)
         }
-        else
-        {
-            //if permission granted then access user contacts
+        else{
             readUserContacts();
         }
     }
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 
         if (requestCode == 100 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
             readUserContacts()
         }
     }
-        private fun readUserContacts()
-    {
-        var source = listOf<String>(
-            ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
-            ContactsContract.CommonDataKinds.Phone.NUMBER
-
-        ).toTypedArray()
+    private fun readUserContacts(){
+        var source = listOf<String>(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME, ContactsContract.CommonDataKinds.Phone.NUMBER).toTypedArray()
 
         var destination = intArrayOf(android.R.id.text1, android.R.id.text2)
 
-        var result_set = contentResolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-            contact_fields, null, null, ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME)
+        var result_set =
+            contentResolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, contact_fields, null, null, ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME)
 
-        var adapter = SimpleCursorAdapter(this, android.R.layout.simple_list_item_2, result_set,
-            source, destination, 0);
+        var adapter = SimpleCursorAdapter(this, android.R.layout.simple_list_item_2, result_set, source, destination, 0);
 
         list_view.adapter = adapter // binding the data with my listview
 
